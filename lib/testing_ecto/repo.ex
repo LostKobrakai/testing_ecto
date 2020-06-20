9430ed7154e871fdd939dc22daea97ca3d8684f4
Benjamin Milde <benni@kobrakai.de>
2020-06-20 14:52:45 +0200

    Move queryable integration into repo

--- lib/testing_ecto/repo.ex
+++ lib/testing_ecto/repo.ex
@@ -3,3 +3,21 @@ defmodule TestingEcto.Repo do
     otp_app: :testing_ecto,
     adapter: Ecto.Adapters.Postgres
 end
+
+defimpl Ecto.Queryable, for: TestingEcto.Accounts.UserQuery do
+  import Ecto.Query
+  alias TestingEcto.Accounts.User
+
+  def to_query(%{filters: filters}) do
+    base = from User, as: :user
+
+    Enum.reduce(filters, base, fn
+      {:name, name}, query ->
+        # DO SANITE NAME for prod
+        from [user: user] in query, where: like(user.name, ^"%#{name}%")
+
+      _, query ->
+        query
+    end)
+  end
+end
